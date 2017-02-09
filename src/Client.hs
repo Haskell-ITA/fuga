852d032d1ee57eebfd35c4a36dc29ec276f1b5c9
Francesco Gazzetta <francygazz@gmail.com>
2017-02-09 13:12:54 +0100

    basic rendering

--- src/Client.hs
+++ src/Client.hs
@@ -4,22 +4,35 @@ import Graphics.Gloss.Interface.IO.Game
 import Control.Concurrent
 import Control.Concurrent.MVar
 import Network.WebSockets
-import Data.Map (empty)
+import Data.Map as Map
+import Data.Maybe (fromMaybe)
+import Data.Monoid
 
 import Types
 
 ip = "127.0.0.1"
 port = 8888
 
+width = 800
+height = 600
+cellSize = 20
+
+data World = World { wDirection :: (Int,Int)
+                   , wGrid :: MVar Grid
+                   , wUuid :: UUID }
+
 main :: IO ()
 main = do
-  world <- newMVar empty
+  grid <- newMVar empty
+  let world = World (0,0) grid
   {-withSocketsDo $ -}
   runClient ip port "/" $ app world
 
-app world conn = do
-  forkIO $ readUpdateWorld world conn
-  playIO (InWindow "fuga" (50, 50) (800, 600))
+app world' conn = do
+  uuid <- undefined
+  let world = world' uuid
+  forkIO $ readUpdateGrid (wGrid world) conn
+  playIO (InWindow "fuga" (50, 50) (width, height))
          black
          60
          world
@@ -27,21 +40,26 @@ app world conn = do
          (handleInput conn)
          step
 
-readUpdateWorld :: MVar Grid -> Connection -> IO ()
-readUpdateWorld mgrid conn = do
+readUpdateGrid :: MVar Grid -> Connection -> IO ()
+readUpdateGrid mgrid conn = do
   undefined --TODO leggi i messaggi e aggiorna il mondo
-  readUpdateWorld mgrid conn
+  readUpdateGrid mgrid conn
 
 step = const $ return . id
 
 handleInput conn event = do
   undefined --TODO invia le mosse al server
 
-render :: MVar Grid -> IO Picture
-render mgrid = do
-  grid <- readMVar mgrid
-  return $ render' grid
+render :: World -> IO Picture
+render world = do
+  grid <- readMVar $ wGrid world
+  return $ render' (wUuid world) grid
 
-render' :: Grid -> Picture
-render' grid = undefined --TODO disegna il mondo
+render' :: UUID -> Grid -> Picture
+render' uuid grid' = foldMap makePlayerMarker grid
+                  <> fromMaybe mempty (makeOwnPlayerMarker <$> Map.lookup uuid grid)
+                  <> circleSolid 3 -- just to see the center
+  where grid = (\(x, y) -> (fromIntegral x * cellSize, fromIntegral y * cellSize)) <$>  grid'
+        makeOwnPlayerMarker (x, y) = translate x y $ color red $ circleSolid (cellSize/3)
+        makePlayerMarker (x, y) = translate x y $ circle (cellSize/2)
 
